e72bff2e13f08c2f594c6b810be50416b475917f
zanand <zakharchenko.tolik@list.ru>
2024-11-02 22:45:43 +0600

    +quiz_question v1.4

--- tg_bot/modules/quiz/quiz_question.py
+++ tg_bot/modules/quiz/quiz_question.py
@@ -1,16 +1,59 @@
 # Структура квиза
 quiz_data = [
     {
-        'question': 'Что такое Python?',
+        'question': '1. Что такое Python?',
         'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
         'correct_option': 0
     },
     {
-        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'question': '2. Какой тип данных используется для хранения целых чисел?',
         'options': ['int', 'float', 'str', 'natural'],
         'correct_option': 0
     },
+    
     # Добавьте другие вопросы
+
+    {
+        'question': '3. Что относится к ООП? ',
+        'options': ['ip-адрес', 'инкапсуляция', 'оптоволокно', 'Ни один из вышеперечисленных'],
+        'correct_option': 1
+    },
+
+    {
+        'question': '4. Каким образом можно преобразовать строковую переменную 2, в целое число 2?',
+        'options': ['integer(2)', 'int(2)', 'castToInt(2)', 'castToInteger(2)'],
+        'correct_option': 1
+    },
+    {
+        'question': '5. Каким образом можно узнать к какому типу переменных относится переменная example?',
+        'options': ['getType(example)', 'type(example)', 'Type(example)', 'example.type'],
+        'correct_option': 1
+    },
+    {
+        'question': '6. Какое из нижеприведенных выражений является допустимым для списка в Python?  ',
+        'options': ['sampleList = [1,2,3,4,5]', 'sampleList = /1,2,3,4,5/', 'sampleList = {1,2,3,4,5}', 'sampleList = (1,2,3,4,5)'],
+        'correct_option': 0
+    },
+    {
+        'question': '7. Какое из нижеприведенных выражений является допустимым для кортежей в Python? ',
+        'options': ['sampleList = [1,2,3,4,5]', 'sampleList = /1,2,3,4,5/', 'sampleList = {1,2,3,4,5}', 'sampleList = (1,2,3,4,5)'],
+        'correct_option': 2
+    },
+    {
+        'question': '8. Какая из следующих функций преобразует строку в список в Python?  ',
+        'options': ['repr(my_string)', 'list(my_string)', 'tuple(my_string)', 'eval(my_string)'],
+        'correct_option': 1
+    },
+    {
+        'question': '9. Какая из следующих функций используется для проверки, все ли символы в строке находятся в нижнем регистре (строчные буквы)?',
+        'options': ['istitle()', 'isnumeric()', 'isspace()', 'islower()'],
+        'correct_option': 3
+    },
+    {
+        'question': '10. Какой оператор осуществляет деление без остатка (возвращает целочисленное значение)?',
+        'options': ['оператор /', 'оператор //', 'оператор %', 'Ни один из вышеперечисленных'],
+        'correct_option': 1
+    }
 ]
 
 
